1aaacd20144d7af3f151dd1f0aca9dccc9030ce3
Yubeshi <116851852+Yu-be-shi@users.noreply.github.com>
2022-10-28 10:23:59 +0900

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -4,26 +4,26 @@ import time
 BLACK_JACK = 21
 Card_Number = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 Card_Mark =  ["♠","♣","♥","♦"]
-Start_Tip = 10000
+Start_bet = 10000
 PLAYER = 0
 DEALER = 1
-END = 1
-NOT_END = 0
 #--------------------------------------------------
 class Me:
-    tip = 0
+    bet = 0
     cards = []
     sum = 0
-    def __init__(self,tip):
-        self.tip = tip
+    def __init__(self,bet):
+        self.bet = bet
 
 class dealer:
     cards = []
     sum = 0
 
-def Bet(m):
-    print("How much to bet？")
+#---------------------------------------------------
+
+def Placing_Bet(m):
     while(True):
+        print("How much to bet？")
         print("→",end="")
         try:
             t = int(input())
@@ -31,7 +31,7 @@ def Bet(m):
         except:
             t = -1
             pass
-        if t >= 0 and m.tip - t >= 0:
+        if t >= 0 and m.bet - t >= 0:
             while(True):
                 print("Really?")
                 print("0:yes,1:no")
@@ -43,30 +43,44 @@ def Bet(m):
                 if really == 0:
                     break
                 elif really == 1:
-                    print("How much to bet？")
                     t = -1
                     break
             if t >= 0:
-                m.tip -= t
+                m.bet -= t
                 break
-        else:
-            print("How much to bet？")
+        print()
     print()
     return t
 
+def Print_Divide(n):
+    if n == 0:
+        print()
+        print("♠"*100)
+        print()
+    if n == 1:
+        print()
+        print("♣"*100)
+        print()
+    if n == 2:
+        print()
+        print("♥"*100)
+        print()
+    if n == 3:
+        print()
+        print("♦"*100)
+        print()
+
 def Start_Call():
-    already_cards = []
     print("Place your bet...")
 
 def Distribute_Card():
-    while(1):
+    while(True):
         num = random.randint(0,51)
         if num in already_cards:
             continue
         already_cards.append(num)
         break
     return num
-
 
 def Cul_Sum(Card):
     sum = 0
@@ -86,7 +100,6 @@ def Cul_Sum(Card):
             sum += 11
         else:
             sum += 1
-
     return sum
 
 def Find_Mark_And_Number(N):
@@ -106,11 +119,8 @@ def Player_Or_Dealer(PoD,Card):
     print()
 
 def Game(me,de):
-    end = 0
     me.cards = []
     de.cards = []
-    #最初の手順
-    print()
     me.cards.append(Distribute_Card())
     me.cards.append(Distribute_Card())
     me.sum = Cul_Sum(me.cards)
@@ -120,14 +130,13 @@ def Game(me,de):
     de.sum = Cul_Sum(de.cards)
 
     #player
-    print("---------------------------------------------------------------------------------------------------")
+    print("-" * 100)
     Player_Or_Dealer(PLAYER,me.cards)
-    print("Dealer: ",end = "")
-    Find_Mark_And_Number(de.cards[0])
+    Player_Or_Dealer(DEALER,[de.cards[0]])
+    print("-" * 100)
     print()
-    print("---------------------------------------------------------------------------------------------------")
     while(True):
-        print("hit(0) or end(1)?")
+        print("0:Hit or 1:End?")
         print("→",end="")
         try:
             hit_or_end = int(input())
@@ -140,66 +149,85 @@ def Game(me,de):
                 break
             #hit
             elif hit_or_end == 0:
-                print("---------------------------------------------------------------------------------------------------")
+                print("-" * 100)
                 me.cards.append(Distribute_Card())
                 me.sum = Cul_Sum(me.cards)
                 Player_Or_Dealer(PLAYER,me.cards)
-                print("Dealer: ",end = "")
-                Find_Mark_And_Number(de.cards[0])
+                Player_Or_Dealer(DEALER,[de.cards[0]])
+                print("-" * 100)
                 print()
-                print("---------------------------------------------------------------------------------------------------")
                 if me.sum > BLACK_JACK:
-                    end = END
-                    print("Result:You Lose")
+                    time.sleep(3)
+                    Print_Divide(1)
+                    print("Bust!Your point:{0}".format(me.sum))
+                    print("Result: You lose")
                     return 0
         else:
-            print("enter the key 0 or 1")
-
-    if end != END:
-        #dealer
-        while(True):
-            print("---------------------------------------------------------------------------------------------------")
-            Player_Or_Dealer(PLAYER,me.cards)
-            Player_Or_Dealer(DEALER,de.cards)
-            print("---------------------------------------------------------------------------------------------------")
-            if de.sum < 17:
-                de.cards.append(Distribute_Card())
-                de.sum = Cul_Sum(de.cards)
-                print()
-            else:
-                print()
-                break
-
-        print("Player Point: "+str(me.sum))
-        print("Dealer Point: "+str(de.sum))
+            print("Press the key 0 or 1")
+    print()
+    #dealer
+    print("Next: Dealer's Turn",end="")
+    time.sleep(1)
+    print(".",end="")
+    time.sleep(1)
+    print(".",end="")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print()
+    turn = 1
+    while(True):
+        print("Turn over the card... ({0})".format(turn))
         time.sleep(1)
+        print("-" * 100)
+        Player_Or_Dealer(PLAYER,me.cards)
+        Player_Or_Dealer(DEALER,de.cards)
+        print("-" * 100)
         print()
-
-        #judge
-        if me.sum > de.sum or de.sum > BLACK_JACK:
-            if me.sum == BLACK_JACK:
-                print("★BLACK_JACK★")
-            print("Result:You Win!")
-            return int(BLACK_JACK - abs(BLACK_JACK - me.sum))
-        elif me.sum < de.sum:
-            if de.sum == BLACK_JACK:
-                print("★BLACK_JACK★")
-            print("Result:You Lose")
-            return 0
+        if de.sum < me.sum:
+            turn += 1
+            de.cards.append(Distribute_Card())
+            de.sum = Cul_Sum(de.cards)
         else:
-            print("Result:DRAW")
-            return -1
+            break
 
+    time.sleep(3)
+    Print_Divide(1)
+    print("Player point: "+str(me.sum))
+    print("Dealer point: "+str(de.sum))
+    print()
+    #judge
+    if me.sum > de.sum or de.sum > BLACK_JACK:
+        if me.sum == BLACK_JACK:
+            print("★BLACK_JACK★")
+            print()
+        print("Result: You win!")
+        return int(BLACK_JACK - abs(BLACK_JACK - me.sum))
+    elif me.sum < de.sum:
+        if de.sum == BLACK_JACK:
+            print("★BLACK_JACK★")
+            print()
+        print("Result: You lose")
+        return 0
+    else:
+        print("Result: Draw")
+        return -1
 
-me = Me(Start_Tip)
+#---------------------------------------------------
+
+me = Me(Start_bet)
 de = dealer()
 play_num = 0
 while(True):
-    bet = 0
+    if me.bet <= 0:
+        print("Game over!({0} play)".format(play_num))
+        print()
+        break
+    place_bet = 0
     start = -1
     already_cards = []
-    print("Your tip: ",end="")
-    print("{0}".format(me.tip))
+    print("Your bet: ",end="")
+    print("{0}".format(me.bet))
     print()
     print("Are you ready to start the game?")
     print("0:yes,1:no")
@@ -207,24 +235,31 @@ while(True):
     while(True):
         try:
             start = int(input())
+            print() 
         except:
             start = -1
         if start == 0 or start == 1:
             break
     if start == 1:
-        print("Thanks for Playing!!({0} play)".format(play_num))
+        Print_Divide(2)
+        print("Thanks for playing!!({0} play,point:{1})".format(play_num,me.bet))
+        print()
         break
-    play_num += 1
-    print() 
+
     Start_Call()
-    bet = Bet(me)
+    play_num += 1
+    place_bet = Placing_Bet(me)
+    Print_Divide(0)
     print("Let the games begin!")
+    print()
     result = Game(me,de)
     print()
     if result ==  - 1:
-        print("Return your tip...")
-        print()
+        me.bet += place_bet
+        print("Return your bet...")
+        print("-" * 100)
         continue
-    me.tip += (result * bet)
-    print("You got {0}".format(result * bet))
-    print("---------------------------------------------------------------------------------------------------")
+    _bet = int((result * place_bet)/10)
+    me.bet += _bet
+    print("You got {0}".format(_bet))
+    Print_Divide(3)
